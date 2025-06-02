a934617d4a8a0776ed079d796d04eb9a2fb068fb
Chairulz <86644331+Chairulz@users.noreply.github.com>
2025-06-02 12:44:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
