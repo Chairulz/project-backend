59982f528e4f3741351b8a6a17739030447d9260
Chairulz <86644331+Chairulz@users.noreply.github.com>
2025-06-02 17:00:51 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,6 @@
 import { Sequelize } from "sequelize";
-import { Storage } from '@google-cloud/storage';
-import dotenv from 'dotenv';
-import { Readable } from 'stream';
-
-const storage = new Storage();
-const bucketName = 'projek_tcc';
-const fileName = '/.env';
-
-async function loadEnvFromBucket() {
-  const file = storage.bucket(bucketName).file(fileName);
-  const contents = await file.download();
-  const envStream = Readable.from(contents[0].toString());
-  dotenv.parse(envStream);
-}
-
-await loadEnvFromBucket();
+import dotenv from "dotenv";
+dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
